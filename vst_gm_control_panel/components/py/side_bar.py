e22f9b3d6fdc0bf4ed308000d89c83d017044768
Tommy <58889527+necronaughtycon@users.noreply.github.com>
2024-06-17 04:26:12 -0400

    +

--- vst_gm_control_panel/components/py/side_bar.py
+++ vst_gm_control_panel/components/py/side_bar.py
@@ -1,7 +1,8 @@
 ''' side_bar.py '''
 
 from kivy.properties import BooleanProperty
-from kivymd.uix.navigationdrawer import MDNavigationLayout 
+from kivymd.uix.navigationdrawer import MDNavigationLayout
+from kivymd.uix.button import MDIconButton
 
 
 class SideBar(MDNavigationLayout):
@@ -17,5 +18,29 @@ class SideBar(MDNavigationLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    def closed_nav_button_config(self):
+        '''
+        Closed Nav Buttons.
+        '''
+        return {
+            'main': {
+                'icon': 'home'
+            },
+            'maintenance': {
+                'icon': 'wrench'
+            },
+            'faults': {
+                'icon': 'alert'
+            }
+        }
+
+    def closed_nav_append(self):
+        '''
+        Append Closed Nav Buttons.
+        '''
+        for k, v in self.closed_nav_button_config().items():
+            button = MDIconButton(icon=v['icon'])
+            self.ids.closed_nav.add_widget(button)
+
     def on_kv_post(self, base_widget):
-        print(self.ids.closed_nav.test)
+        self.closed_nav_append()
