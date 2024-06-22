c2a39537e898d23a5842fb25b0b80436a3b2fed5
necronaughtycon <tommyxwallace@outlook.com>
2024-06-22 04:14:09 -0400

    +

--- vst_gm_control_panel/utils/language_handler.py
+++ vst_gm_control_panel/utils/language_handler.py
@@ -44,17 +44,7 @@ class LanguageHandler:
         '''
         self._user_db.add_setting('language', language)
 
-    def switch_language(self, selected_language):
-        '''
-        Purpose:
-        - Switch the language of the app.
-        Parameters:
-        - selected_language: str
-        '''
-        self.language = selected_language
-        self.save_user_language(selected_language)
-
-    def translate(self, key, default=None):
+    def translate_text(self, key, default=None):
         '''
         Purpose:
         - Translate a key to the user's language.
@@ -65,8 +55,52 @@ class LanguageHandler:
         translation = self._translations_db.translate(self.language, key)
         if translation is None:
             self.log('error', f'No translation found for key: {key}')
-            if default is None:
-                default = key
-            translation = default
+            return default
         return key
-            
+
+    def switch_language(self, selected_language):
+        '''
+        Purpose:
+        - Switch the language of the app.
+        Parameters:
+        - selected_language: str
+        '''
+        self.language = selected_language
+        self.save_user_language(selected_language)
+
+    def walk_app_widget_tree(self, widget):
+        '''
+        Purpose:
+        - Walk the app's widget tree and translate text.
+        Parameters:
+        - widget: kivy.uix.widget
+        '''
+        # Check if widget has 'ids' attribute and it's not empty.
+        if hasattr(widget, 'ids') and widget.ids:
+            for key, val in widget.ids.items():
+                # Translate and update text if available.
+                if hasattr(val, 'text'):
+                    translated_text = self.translate(key)
+                if translated_text:
+                    val.text = translated_text.upper()
+                self.udpate_screen_title(key, val)
+        for child in widget.children:
+            self.walk_app_widget_tree(child)
+
+    def update_screen_title(self, key, val):
+        screens = {
+            'main_screen': 'main',
+            'test_screen': 'test'
+        }
+        for screen, screen_name in screens.items():
+            if key == screen:
+                title = self.translate(screen_name, screen_name)
+                val.screen_title = title.upper()
+
+    def check_all_screens(self, *args):
+        '''
+        Purpose:
+        - Check all screens for translations.
+        '''
+        for screen in self.app.sm.screens:
+            self.walk_app_widget_tree(screen)
